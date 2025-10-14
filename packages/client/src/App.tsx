e6deea7bb80476f6ebd04b0358f58481e4e8482e
Andrew Fulton <andrew.fulton@portable.com.au>
2025-10-14 13:12:10 +1100

    Added to landing page

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,14 @@
 function App() {
   return (
-    <h1>Portable News App</h1>
+    <>
+      <h1>Portable News App</h1>
+      
+      <form>
+        <input placeholder="Type something..." />
+      </form>
+      <code>{import.meta.env.VITE_API_BASE }</code>
+    </>
+    
   )
 }
 
